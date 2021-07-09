93d7ea6a6bd5126598c968753ca6d7f270b44f43
vvvhatislove <vladenisov1234@yandex.ru>
2021-07-10 00:51:45 +0300

    pure functions createNewPosts and createNewFeed are set

--- src/formHandler.js
+++ src/formHandler.js
@@ -1,41 +1,61 @@
+import axios from 'axios';
 import _ from 'lodash';
-import parseRss from './parser.js';
-import { validateUrl, getRssData } from './utils.js';
+import parseRss from './rssParser.js';
+import getProxy from './proxy.js';
+import validateUrl from './urlValidator.js';
+
+const createNewFeed = (feedData, url) => ({
+  title: feedData.title,
+  description: feedData.description,
+  url,
+  feedId: _.uniqueId('feed_'),
+});
+
+const createNewPosts = (postsData, feedId) => {
+  const newPosts = postsData.map((postData) => ({
+    title: postData.title,
+    description: postData.description,
+    url: postData.url,
+    postId: _.uniqueId('post_'),
+    feedId,
+    viewed: false,
+  }));
+
+  return newPosts;
+};
 
 export default (watchedState) => function handler(e) {
   e.preventDefault();
   const formData = new FormData(e.target);
-  const currentUrl = formData.get('url-input');
-  const { rssForm } = watchedState;
-  const existingUrls = rssForm.rssData.feeds.map((feed) => feed.url);
-  validateUrl(currentUrl, existingUrls)
+  const url = formData.get('url-input');
+  const feedUrls = watchedState.rssForm.rssData.feeds
+    .map((feed) => feed.url);
+
+  validateUrl(url, feedUrls)
     .then(() => {
-      rssForm.processState = 'sending';
-      return Promise.resolve(getRssData(currentUrl));
+      watchedState.rssForm.processState = 'sending';
+      const proxy = getProxy();
+      const axiosPromise = axios.get(proxy, { params: { url, disableCache: true } })
+        .catch(() => {
+          throw new Error('networkError');
+        });
+      return Promise.resolve(axiosPromise);
     })
-    .then((response) => parseRss(response))
-    .then((parsedRssData) => {
-      rssForm.processState = 'finished';
-      rssForm.currentFeedback = { type: 'successMessage' };
-
-      const oldFeeds = rssForm.rssData.feeds;
-      const newFeed = parsedRssData.feed;
-      const newFeedId = _.uniqueId('feed_');
-      newFeed.feedId = newFeedId;
+    .then((response) => parseRss(response.data.contents))
+    .then((parsedData) => {
+      watchedState.rssForm.processState = 'finished';
+      watchedState.rssForm.currentFeedback = { type: 'successMessage' };
+      const newFeed = createNewFeed(parsedData.feedData, url);
+      const oldFeeds = watchedState.rssForm.rssData.feeds;
       if (oldFeeds) {
-        rssForm.rssData.feeds = [newFeed].concat(oldFeeds);
-      } else rssForm.rssData.feeds = [newFeed];
+        watchedState.rssForm.rssData.feeds = [newFeed].concat(oldFeeds);
+      } else watchedState.rssForm.rssData.feeds = [newFeed];
 
-      const oldPosts = rssForm.rssData.posts;
-      const newPosts = parsedRssData.posts.reduce((acc, newPost) => {
-        newPost.feedId = newFeedId;
-        newPost.postId = _.uniqueId('post_');
-        acc.push(newPost);
-        return acc;
-      }, []);
+      const newPosts = createNewPosts(parsedData.postsData, newFeed.feedId);
+      const oldPosts = watchedState.rssForm.rssData.posts;
       if (oldPosts) {
-        rssForm.rssData.posts = newPosts.concat(oldPosts);
-      } else rssForm.rssData.posts = newPosts;
+        watchedState.rssForm.rssData.posts = newPosts.concat(oldPosts);
+      } else watchedState.rssForm.rssData.posts = newPosts;
     })
     .catch((error) => {
       watchedState.rssForm.processState = 'failed';
