189b28ac15cb17adf8b41aaaaa7fa6b618c31f48
vvvhatislove <vladenisov1234@yandex.ru>
2021-07-14 14:07:21 +0300

    errorRender added

--- src/view.js
+++ src/view.js
@@ -2,14 +2,14 @@ import onChange from 'on-change';
 import processStateHandler from './render/processStateHandler.js';
 import feedsRender from './render/feedsRender.js';
 import postsRender from './render/postsRender.js';
-import feedbackRender from './render/feedbackRender.js';
+import errorRender from './render/errorRender.js';
 import modalRender from './render/modalRender.js';
 
-export default (state, i18nextInstance) => {
+export default (state, i18nextInstance, domElements) => {
   const watchedState = onChange(state, (statePath, currValue) => {
     switch (statePath) {
       case 'rssForm.processState':
-        processStateHandler(currValue, i18nextInstance);
+        processStateHandler(currValue, i18nextInstance, domElements);
         break;
       case 'rssForm.rssData.feeds':
         feedsRender(currValue, i18nextInstance, watchedState);
@@ -17,8 +17,8 @@ export default (state, i18nextInstance) => {
       case 'rssForm.rssData.posts':
         postsRender(currValue, i18nextInstance, watchedState);
         break;
-      case 'rssForm.currentFeedback':
-        feedbackRender(currValue, i18nextInstance);
+      case 'rssForm.error':
+        errorRender(currValue, i18nextInstance);
         break;
       case 'rssForm.currentModal':
         modalRender(currValue, i18nextInstance, watchedState);
