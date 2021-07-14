2cf3f6b9813c5635bc005d18ee1fe636d7939a80
vvvhatislove <vladenisov1234@yandex.ru>
2021-07-14 15:04:51 +0300

    mistakes fixed

--- src/feedUpdatesChecker.js
+++ src/feedUpdatesChecker.js
@@ -17,31 +17,31 @@ const createNewPosts = (postsData, feedId) => {
 };
 
 const checkFeedUpdates = (watchedState, delay) => {
-  const { feeds } = watchedState.rssForm.rssData;
+  const { feeds } = watchedState.rssData;
   const proxy = getProxy();
   const promises = feeds.map((feed) => {
     const { url } = feed;
     const promise = axios.get(proxy, { params: { url, disableCache: true } })
       .then((response) => parseRss(response.data.contents))
       .then((parsedData) => {
-        const oldPosts = watchedState.rssForm.rssData.posts;
+        const oldPosts = watchedState.rssData.posts;
         const loadedPosts = parsedData.postsData;
         const diffPostsArray = _.differenceWith(loadedPosts, oldPosts,
           (a, b) => a.title === b.title);
         const newPosts = createNewPosts(diffPostsArray, feed.feedId);
         if (oldPosts) {
-          watchedState.rssForm.rssData.posts = newPosts.concat(oldPosts);
+          watchedState.rssData.posts = newPosts.concat(oldPosts);
         } else {
-          watchedState.rssForm.rssData.posts = newPosts;
+          watchedState.rssData.posts = newPosts;
         }
       });
     return promise;
   });
   Promise.all(promises)
     .catch((error) => {
-      watchedState.rssForm.processState = 'failed';
-      watchedState.rssForm.error = error;
-      watchedState.rssForm.processState = 'filling';
+      watchedState.processState = 'failed';
+      watchedState.error = error;
+      watchedState.processState = 'filling';
     })
     .finally(() => {
       setTimeout(() => checkFeedUpdates(watchedState, delay), delay);
