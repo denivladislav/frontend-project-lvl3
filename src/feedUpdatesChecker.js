d4d3d5a09ab8cc804d0ea28dbbca60acf024bff9
vvvhatislove <vladenisov1234@yandex.ru>
2021-07-14 14:30:56 +0300

    updatesChecker renamed to feedUpdatesChecker

--- src/feedUpdatesChecker.js
+++ src/feedUpdatesChecker.js
@@ -16,16 +16,12 @@ const createNewPosts = (postsData, feedId) => {
   return newPosts;
 };
 
-const checkFeedUpdates = (watchedState) => {
+const checkFeedUpdates = (watchedState, delay) => {
   const { feeds } = watchedState.rssForm.rssData;
-  feeds.forEach((feed) => {
-    const proxy = getProxy();
+  const proxy = getProxy();
+  const promises = feeds.map((feed) => {
     const { url } = feed;
-    const axiosPromise = axios.get(proxy, { params: { url, disableCache: true } })
-      .catch(() => {
-        throw new Error('networkError');
-      });
-    return Promise.resolve(axiosPromise)
+    const promise = axios.get(proxy, { params: { url, disableCache: true } })
       .then((response) => parseRss(response.data.contents))
       .then((parsedData) => {
         const oldPosts = watchedState.rssForm.rssData.posts;
@@ -35,18 +31,21 @@ const checkFeedUpdates = (watchedState) => {
         const newPosts = createNewPosts(diffPostsArray, feed.feedId);
         if (oldPosts) {
           watchedState.rssForm.rssData.posts = newPosts.concat(oldPosts);
-        } else watchedState.rssForm.rssData.posts = newPosts;
-      })
-      .catch((error) => {
-        watchedState.rssForm.processState = 'failed';
-        watchedState.rssForm.currentFeedback = {
-          type: 'errorMessage',
-          tag: error.message,
-        };
-        watchedState.rssForm.processState = 'filling';
+        } else {
+          watchedState.rssForm.rssData.posts = newPosts;
+        }
       });
+    return promise;
   });
-  setTimeout(() => checkFeedUpdates(watchedState), 5000);
+  Promise.all(promises)
+    .catch((error) => {
+      watchedState.rssForm.processState = 'failed';
+      watchedState.rssForm.error = error;
+      watchedState.rssForm.processState = 'filling';
+    })
+    .finally(() => {
+      setTimeout(() => checkFeedUpdates(watchedState, delay), delay);
+    });
 };
 
 export default checkFeedUpdates;
