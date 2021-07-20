188cdf01ddf12a9be3d0da815b8707cf9ad221ad
vvvhatislove <vladenisov1234@yandex.ru>
2021-07-20 20:21:12 +0300

    constants renamed

--- src/render/feedsRender.js
+++ src/render/feedsRender.js
@@ -1,19 +1,19 @@
 export default (feeds, i18nextInstance, domElements) => {
-  const feedsDiv = domElements.feeds;
-  feedsDiv.innerHTML = `
+  const feedsCard = domElements.feeds;
+  feedsCard.innerHTML = `
     <div class="card-body">
       <h2 class="h4 m-0">${i18nextInstance.t('headers.feeds')}</h2>
     </div>  
   `;
-  const feedsUl = document.createElement('ul');
-  feedsUl.classList.add('list-group', 'border-0', 'rounded-0');
+  const feedsList = document.createElement('ul');
+  feedsList.classList.add('list-group', 'border-0', 'rounded-0');
 
-  feedsUl.innerHTML = feeds.map((feed) => `
+  feedsList.innerHTML = feeds.map((feed) => `
       <li class="list-group-item border-0 border-end-0">
         <h3 class="h6 m-0">${feed.title}</h3>
         <p class="m-0 small text-black-50">${feed.description}</p>
       </li>
     `);
 
-  feedsDiv.append(feedsUl);
+  feedsCard.append(feedsList);
 };
