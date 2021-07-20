2f9ee4cee304c9eff1a35a05bb9e6ecbaa3c08de
vvvhatislove <vladenisov1234@yandex.ru>
2021-07-20 20:10:55 +0300

    mistakes fixed

--- src/render/feedsRender.js
+++ src/render/feedsRender.js
@@ -8,8 +8,7 @@ export default (feeds, i18nextInstance, domElements) => {
   const feedsUl = document.createElement('ul');
   feedsUl.classList.add('list-group', 'border-0', 'rounded-0');
 
-  feedsUl.innerHTML = feeds
-    .map((feed) => `
+  feedsUl.innerHTML = feeds.map((feed) => `
       <li class="list-group-item border-0 border-end-0">
         <h3 class="h6 m-0">${feed.title}</h3>
         <p class="m-0 small text-black-50">${feed.description}</p>
