ebd45501a914e63cd308d84b4ddf6531012b2d27
vvvhatislove <vladenisov1234@yandex.ru>
2021-07-26 18:02:40 +0300

    if/else instead of switch

--- src/render/errorRender.js
+++ src/render/errorRender.js
@@ -1,19 +1,19 @@
+const getErrorMessage = (error, i18nextInstance) => {
+  if (error.name === 'ValidationError') {
+    return i18nextInstance.t(`errors.${error.message}`);
+  }
+  if (error.isRssParseError) {
+    return i18nextInstance.t('errors.invalidRss');
+  }
+  if (error.isAxiosError) {
+    return i18nextInstance.t('errors.networkError');
+  }
+  return i18nextInstance.t('errors.unknownError');
+};
+
 export default (error, i18nextInstance, domElements) => {
   const { feedback } = domElements;
   feedback.classList.remove('text-success');
   feedback.classList.add('text-danger');
-  switch (true) {
-    case error.name === 'ValidationError':
-      feedback.textContent = i18nextInstance.t(`errors.${error.message}`);
-      break;
-    case error.isRssParseError:
-      feedback.textContent = i18nextInstance.t('errors.invalidRss');
-      break;
-    case error.isAxiosError:
-      feedback.textContent = i18nextInstance.t('errors.networkError');
-      break;
-    default:
-      feedback.textContent = i18nextInstance.t('errors.unknownError');
-      break;
-  }
+  feedback.textContent = getErrorMessage(error, i18nextInstance);
 };
