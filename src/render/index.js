a26a04d186cdd8eb1c00e3f227a823eca719296f
vvvhatislove <vladenisov1234@yandex.ru>
2021-07-26 19:10:34 +0300

    render modules names changed

--- src/render/index.js
+++ src/render/index.js
@@ -1,6 +1,6 @@
-export { feedsRender } from './feedsRenderer.js';
-export { postsRender } from './postsRenderer.js';
-export { errorRender } from './errorRenderer.js';
-export { modalRender } from './modalRenderer.js';
+export { feedsRenderer } from './feedsRenderer.js';
+export { postsRenderer } from './postsRenderer.js';
+export { errorRenderer } from './errorRenderer.js';
+export { modalRenderer } from './modalRenderer.js';
 export { processStateHandler } from './processStateHandler.js';
-export { viewedPostsRender } from './viewedPostsRenderer.js';
+export { viewedPostsRenderer } from './viewedPostsRenderer.js';
