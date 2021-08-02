5f521334d9d6b798f842e5ff800c10d5c7ee6cd9
vvvhatislove <vladenisov1234@yandex.ru>
2021-08-02 17:23:38 +0300

    feeds and posts are now prepared in addNewFeedData function

--- src/formSubmitHandler.js
+++ src/formSubmitHandler.js
@@ -1,34 +1,25 @@
 import axios from 'axios';
 import _ from 'lodash';
-import parseFeedData from './rssParser.js';
+import parseNewFeedData from './rssParser.js';
 import getProxyUrl from './getProxyUrl.js';
 import validateUrl from './urlValidator.js';
 
-const createNewFeed = (data, url) => ({
-  ...data,
-  url,
-  id: _.uniqueId('feed_'),
-});
-
-const createNewPosts = (data, feedId) => data.items.map((item) => ({
-  ...item,
-  id: _.uniqueId('post_'),
-  feedId,
-}));
-
-const getFeedData = (url) => {
-  const proxyUrl = getProxyUrl(url);
-  return axios.get(proxyUrl);
-};
-
-const updateState = (parsedData, watchedState, url) => {
+const addNewFeedData = (parsedData, watchedState, url) => {
   watchedState.processState = 'finished';
 
-  const newFeed = createNewFeed(parsedData, url);
+  const newFeed = {
+    ...parsedData,
+    url,
+    id: _.uniqueId('feed_'),
+  };
   const oldFeeds = watchedState.rssData.feeds;
   watchedState.rssData.feeds = [newFeed, ...oldFeeds];
 
-  const newPosts = createNewPosts(parsedData, newFeed.id);
+  const newPosts = parsedData.items.map((item) => ({
+    ...item,
+    id: _.uniqueId('post_'),
+    feedId: newFeed.id,
+  }));
   const oldPosts = watchedState.rssData.posts;
   watchedState.rssData.posts = [...newPosts, ...oldPosts];
 };
@@ -47,9 +38,10 @@ const handleFormSubmit = (event, watchedState) => {
     return;
   }
 
-  getFeedData(url)
-    .then((response) => parseFeedData(response.data.contents))
-    .then((parsedData) => updateState(parsedData, watchedState, url))
+  const proxyUrl = getProxyUrl(url);
+  axios.get(proxyUrl)
+    .then((response) => parseNewFeedData(response.data.contents))
+    .then((parsedData) => addNewFeedData(parsedData, watchedState, url))
     .catch((error) => {
       watchedState.processState = 'failed';
       watchedState.error = error;
