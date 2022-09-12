85b6ad1de4469504797c4b8b33f9bdf368af6aa9
vvvhatislove <vladenisov1234@yandex.ru>
2022-09-12 11:36:16 +0300

    proxyUrl updated

--- src/getProxyUrl.js
+++ src/getProxyUrl.js
@@ -1,5 +1,5 @@
 export default (url) => {
-  const proxyUrl = new URL('https://hexlet-allorigins.herokuapp.com/get?');
+  const proxyUrl = new URL('https://allorigins.hexlet.app/get?');
   proxyUrl.searchParams.append('url', url);
   proxyUrl.searchParams.append('disableCache', true);
   return proxyUrl;
